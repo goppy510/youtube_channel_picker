7d96455a82c5b16a9e43ab9a8443baaec771e59c
takumi-goto <tgoto@eviry.com>
2023-06-23 18:26:05 +0900

    fix

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
     if (tab.url.startsWith('https://www.youtube.com/@')) {
       // アクティブなタブが更新されてHTMLが完全に読み込まれ、かつYouTubeチャンネルページを表示している
-      chrome.action.setIcon({tabId: tabId, path: './src/assets/play_button_48.png'});
+      chrome.action.setIcon({tabId: tabId, path: './src/assets/icon_64.png'});
       chrome.action.setPopup({ tabId: tabId, popup: './src/popup/popup.html' });
     } else {
       // アクティブなタブが更新されてHTMLが完全に読み込まれ、かつ非対象ページを表示している
-      chrome.action.setIcon({tabId: tabId, path: './src/assets/play_button_gray.png'});
+      chrome.action.setIcon({tabId: tabId, path: './src/assets/icon_64_gray.png'});
       chrome.action.setPopup({ tabId: tabId, popup: './src/popup/not-found.html' });
     }
   }
